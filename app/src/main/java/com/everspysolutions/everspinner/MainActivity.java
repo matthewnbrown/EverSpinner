fac3fc2984ea1ee84ef0b5681bb465eccf3afb77
Matt Brown <gitmattb@gmail.com>
2020-09-23 16:49:54 -0700

    Added spinner icon Added bottom nav bar to move between spinner and saved text

--- app/src/main/java/com/everspysolutions/everspinner/MainActivity.java
+++ app/src/main/java/com/everspysolutions/everspinner/MainActivity.java
@@ -1,10 +1,12 @@
 package com.everspysolutions.everspinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
@@ -12,6 +14,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -29,19 +32,23 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private NavController navController ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        NavHostFragment navHostFragment =
+                (NavHostFragment) supportFragmentManager.findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setSelectedItemId(R.id.spinner_container);
 
+        NavigationUI.setupWithNavController(bottomNav, navController);
+        System.out.println("STARTED");
 
-//        FragmentManager supportFragmentManager = getSupportFragmentManager();
-//        NavHostFragment navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment);
-//        NavController navController = navHostFragment.getNavController();
-//        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
-//        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 
 

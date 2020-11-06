5ffdd2d805efe513da94a70a4e2697ddfb0cefc2
Matt Brown <gitmattb@gmail.com>
2020-11-06 14:45:55 -0800

    Minor cleanup of unused function

--- app/src/main/java/com/everspysolutions/everspinner/SynonymList/SynonymExpandableListFragment.java
+++ app/src/main/java/com/everspysolutions/everspinner/SynonymList/SynonymExpandableListFragment.java
@@ -35,8 +35,6 @@ public class SynonymExpandableListFragment extends Fragment {
 
     ExpandableListAdapter listAdapter;
     ExpandableListView expListView;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -87,10 +85,11 @@ public class SynonymExpandableListFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_synonym_item_list, container, false);
 
         expListView = rootView.findViewById(R.id.synonym_expandable_list);
-        prepareListData();
+
         SynonymCacher synonymCacher = new SynonymCacher();
         Random random = new Random();
 
+        // Generating Random Data for testing.
         for(int i = 0; i < 6; i++) {
             int childCount = random.nextInt(6);
             Synonym [] synonyms = new Synonym[childCount];
@@ -114,42 +113,4 @@ public class SynonymExpandableListFragment extends Fragment {
 
     }
 
-    private void prepareListData() {
-        listDataHeader = new ArrayList<String>();
-        listDataChild = new HashMap<String, List<String>>();
-
-        // Adding child data
-        listDataHeader.add("Top 250");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
-
-        // Adding child data
-        List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
-
-        List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
-
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
-
-        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
-    }
 }

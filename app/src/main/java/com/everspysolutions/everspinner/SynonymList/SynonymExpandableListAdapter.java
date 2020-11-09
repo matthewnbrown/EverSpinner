8b02eb3159ef22eca0f811b6756d3e077aedb2c6
Matt Brown <gitmattb@gmail.com>
2020-11-09 14:10:53 -0800

    Add add symbol drawable Update Synonym list fragment layout Update Synonym List group layout Update Synonym List item layout Add new strings to strings.xml

--- app/src/main/java/com/everspysolutions/everspinner/SynonymList/SynonymExpandableListAdapter.java
+++ app/src/main/java/com/everspysolutions/everspinner/SynonymList/SynonymExpandableListAdapter.java
@@ -1,20 +1,26 @@
 package com.everspysolutions.everspinner.SynonymList;
 
 import android.content.Context;
-import android.graphics.Typeface;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.text.InputType;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
 
 import com.everspysolutions.everspinner.R;
 import com.everspysolutions.everspinner.SynonymFinder.Synonym;
 import com.everspysolutions.everspinner.SynonymFinder.SynonymCacher;
 
-import org.w3c.dom.Text;
-
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -22,6 +28,7 @@ public class SynonymExpandableListAdapter extends BaseExpandableListAdapter {
 
     private final SynonymCacher synonymCacher;
     private List<String> synonymBaseWords;
+
 
     public SynonymExpandableListAdapter(SynonymCacher synonymCacher) {
         this.synonymCacher = synonymCacher;
@@ -45,19 +52,28 @@ public class SynonymExpandableListAdapter extends BaseExpandableListAdapter {
 
         final Synonym synonym = (Synonym) getChild(groupPosition, childPosition);
 
+        ChildViewHolder holder;
+
         if (convertView == null) {
             LayoutInflater infalInflater = (LayoutInflater) parent.getContext()
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = infalInflater.inflate(R.layout.synonym_list_item, null);
+            convertView = infalInflater.inflate(R.layout.synonym_list_item,  parent, false);
+
+            holder = new ChildViewHolder(convertView, groupPosition, childPosition, synonym.getWord());
+            convertView.setTag(holder);
+
+        } else {
+            holder = (ChildViewHolder) convertView.getTag();
         }
 
-        TextView txtListChild = (TextView) convertView
-                .findViewById(R.id.synonym_item_label);
-        TextView txtListScore = (TextView) convertView
-                .findViewById(R.id.synonym_item_num);
 
-        txtListChild.setText(synonym.getWord());
-        txtListScore.setText(Integer.toString(synonym.getScore()));
+        holder.lblSynonym.setText(synonym.getWord());
+        holder.mScoreText.setText(Integer.toString(synonym.getScore()));
+
+        //holder.btnDeleteSyn.setOnClickListener(view -> );
+        holder.btnEditSyn.setOnClickListener(view -> onEditSynonymClick(view, null, holder));
+
+
         return convertView;
     }
 
@@ -87,16 +103,32 @@ public class SynonymExpandableListAdapter extends BaseExpandableListAdapter {
 
         String headerTitle = (String) getGroup(groupPosition);
 
+        GroupViewHolder holder;
+
         if (convertView == null) {
             LayoutInflater infalInflater = (LayoutInflater) parent.getContext()
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = infalInflater.inflate(R.layout.synonym_item_list_group, null);
+            convertView = infalInflater.inflate(R.layout.synonym_item_list_group, parent, false);
+
+            holder = new GroupViewHolder(convertView, groupPosition);
+            convertView.setTag(holder);
+        } else {
+            holder = (GroupViewHolder) convertView.getTag();
         }
 
-        TextView lblListHeader = convertView
-                .findViewById(R.id.synonym_item_header);
+        // Prevent group from not being expandable due to focusable button
+        holder.newSynBtn.setFocusable(false);
 
-        lblListHeader.setText(headerTitle);
+        holder.newSynBtn.setOnClickListener(
+                view -> onNewSynonymClick(view, holder)
+        );
+
+        holder.delWordBtn.setFocusable(false);
+        holder.delWordBtn.setOnClickListener(
+                view -> onDeleteBaseWordClick(view, holder)
+        );
+
+        holder.lblListHeader.setText(headerTitle);
 
         return convertView;
     }
@@ -110,5 +142,190 @@ public class SynonymExpandableListAdapter extends BaseExpandableListAdapter {
     public boolean isChildSelectable(int groupPosition, int childPosition) {
         return true;
     }
+
+    public void synonymEditAlert(View view, GroupViewHolder pHolder, ChildViewHolder cHolder) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+
+        LayoutInflater inflater = (LayoutInflater) view.getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        final View dialoglayout = inflater.inflate(R.layout.synonym_new_syn_alert, null);
+        final EditText wordTxt = dialoglayout.findViewById(R.id.new_syn_word_input);
+        final EditText scoreTxt = dialoglayout.findViewById(R.id.new_syn_score_input);
+
+        final String title = cHolder == null ? "Edit Synonym" : "New Synonym";
+        builder.setTitle(title);
+
+        if(cHolder != null) {
+            wordTxt.setFocusable(false);
+            wordTxt.setEnabled(false);
+            wordTxt.setCursorVisible(false);
+            wordTxt.setKeyListener(null);
+            wordTxt.setBackgroundColor(Color.TRANSPARENT);
+            wordTxt.setTextColor(Color.BLACK);
+
+            Synonym syn = (Synonym) getChild(cHolder.groupPos, cHolder.childPos);
+            wordTxt.setText((syn.getWord()));
+            scoreTxt.setText(Integer.toString(syn.getScore()));
+        }
+
+        builder.setView(dialoglayout);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(!TextUtils.isDigitsOnly(scoreTxt.getText())
+                        || scoreTxt.getText().toString().length() == 0) {
+                    return;
+                }
+
+                String word = wordTxt.getText().toString().toLowerCase().trim();
+                int score = Integer.parseInt(scoreTxt.getText().toString());
+
+                if(cHolder != null){
+                    synonymCacher.updateSynonymScore((String) getGroup(cHolder.groupPos), word, score);
+                } else {
+                    synonymCacher.addSynonymToBaseWord((String) getGroup(pHolder.groupPos), word, score);
+                }
+                notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+
+        builder.show();
+    }
+
+    public void onNewSynonymClick(View view, GroupViewHolder holder) {
+        synonymEditAlert(view, holder, null);
+    }
+
+    public void  onEditSynonymClick(View view, GroupViewHolder pHolder, ChildViewHolder cHolder) {
+        synonymEditAlert(view, pHolder, cHolder);
+    }
+
+
+    public void onDeleteBaseWordClick(View view, GroupViewHolder holder) {
+        Context ctx = view.getContext();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
+        builder.setMessage
+                ("Are you sure you want to delete \"" + ( (String) getGroup(holder.groupPos)) +"\"?");
+        builder.setCancelable(true);
+
+        builder.setPositiveButton(
+                "Yes",
+                (dialog, id) -> {
+                    String baseWord = (String) getGroup(holder.groupPos);
+                    synonymCacher.removeBaseWord(baseWord);
+                    this.synonymBaseWords = synonymCacher.getBaseWords();
+
+                    CharSequence text = "Deleted base word.";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(ctx, text, duration);
+                    toast.show();
+
+                    dialog.cancel();
+                    notifyDataSetChanged();
+                });
+
+        builder.setNegativeButton(
+                "No",
+                (dialog, id) -> dialog.cancel());
+
+        builder.show();
+    }
+
+    // Unused/
+    public void onDeleteSynonymClick(View view, ChildViewHolder holder) {
+        Context ctx = view.getContext();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
+        final Synonym synonym = (Synonym) getChild(holder.groupPos, holder.childPos);
+        builder.setMessage
+                ("Are you sure you want to delete \""
+                        + synonym.getWord() +"\"?");
+        builder.setCancelable(true);
+
+        builder.setPositiveButton(
+                "Yes",
+                (dialog, id) -> {
+                    String baseWord = (String) getGroup(holder.groupPos);
+                    // Delete
+
+                    CharSequence text = "Deleted synonym.";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(ctx, text, duration);
+                    toast.show();
+
+                    dialog.cancel();
+                    notifyDataSetChanged();
+                });
+
+        builder.setNegativeButton(
+                "No",
+                (dialog, id) -> dialog.cancel());
+
+        builder.show();
+    }
+
+    public static class GroupViewHolder
+    {
+        public final View mView;
+        public final TextView lblListHeader;
+        public final ImageButton newSynBtn;
+        public final ImageButton delWordBtn;
+        public final int groupPos;
+
+        public GroupViewHolder(View view, int groupPos)
+        {
+            this.mView = view;
+            this.lblListHeader = view.findViewById(R.id.synonym_item_header);
+            this.newSynBtn = view.findViewById(R.id.btn_add_new_synonym);
+            this.delWordBtn = view.findViewById(R.id.btn_delete_baseword);
+            this.groupPos = groupPos;
+
+        }
+
+    }
+
+    public static class ChildViewHolder
+    {
+        public final View mView;
+        public final TextView mScoreText;
+        public final TextView lblSynonym;
+        public final ImageButton btnEditSyn;
+        public final ImageButton btnDeleteSyn;
+        public final int groupPos;
+        public final int childPos;
+        public final String synonym;
+
+        public ChildViewHolder(View view, int groupPos, int childPos, String synonym)
+        {
+            this.mView = view;
+            this.mScoreText = view.findViewById(R.id.synonym_item_score);
+            this.lblSynonym = view.findViewById(R.id.synonym_item_label);
+            this.btnEditSyn = view.findViewById(R.id.btn_edit_syn);
+            this.btnDeleteSyn = view.findViewById(R.id.btn_delete_syn);
+            this.groupPos = groupPos;
+            this.childPos = childPos;
+            this.synonym = synonym;
+        }
+
+    }
+
+    public void addBaseWord(String word) {
+        synonymCacher.addItemToCache(word, new ArrayList<>());
+        this.synonymBaseWords = synonymCacher.getBaseWords();
+        notifyDataSetChanged();
+    }
+
 
 }
--- app/src/main/java/com/everspysolutions/everspinner/SynonymList/SynonymExpandableListFragment.java
+++ app/src/main/java/com/everspysolutions/everspinner/SynonymList/SynonymExpandableListFragment.java
@@ -1,10 +1,12 @@
 package com.everspysolutions.everspinner.SynonymList;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,8 +15,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ImageButton;
 
 import com.everspysolutions.everspinner.R;
 import com.everspysolutions.everspinner.SavedFileRecyclerViewAdapter;
@@ -33,14 +37,13 @@ import java.util.Random;
  */
 public class SynonymExpandableListFragment extends Fragment {
 
-    ExpandableListAdapter listAdapter;
+    SynonymExpandableListAdapter listAdapter;
     ExpandableListView expListView;
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String SYNONYM_CACHER_KEY = "synonym_cacher_key";
 
-    // TODO: Rename and change types of parameters
+
     private SynonymCacher synonymCacher;
 
     public SynonymExpandableListFragment() {
@@ -54,12 +57,11 @@ public class SynonymExpandableListFragment extends Fragment {
      * @param synonymCacher Parameter 1.
      * @return A new instance of fragment SynonymListFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static SynonymExpandableListFragment newInstance(SynonymCacher synonymCacher) {
         SynonymExpandableListFragment fragment = new SynonymExpandableListFragment();
         Bundle args = new Bundle();
-        //args.put(SYNONYM_CACHER_KEY, synonymCacher);
-        //args.putString(ARG_PARAM1, param1);
+        //args.putSerializable(SYNONYM_CACHER_KEY, synonymCacher);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -75,23 +77,17 @@ public class SynonymExpandableListFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_synonym_item_list, container, false);
 
+        synonymCacher = SynonymExpandableListFragmentArgs
+                .fromBundle(getArguments()).getSynonymCacher();
+
         expListView = rootView.findViewById(R.id.synonym_expandable_list);
 
-        SynonymCacher synonymCacher = new SynonymCacher();
-        Random random = new Random();
-
-        // Generating Random Data for testing.
-        for(int i = 0; i < 6; i++) {
-            int childCount = random.nextInt(6);
-            ArrayList<Synonym> synonyms = new ArrayList<>();
-            for (int j = 0; j < childCount; j++) {
-                synonyms.add(new Synonym(Integer.toString(j), random.nextInt(10000)));
-            }
-            synonymCacher.addItemToCache(Integer.toString(i), synonyms);
-        }
         listAdapter = new SynonymExpandableListAdapter(synonymCacher);
         expListView.setAdapter(listAdapter);
         expListView.setGroupIndicator(null);
+
+        ImageButton newBaseWordBtn = rootView.findViewById(R.id.btn_add_new_baseword);
+        newBaseWordBtn.setOnClickListener(view -> onNewBaseWordClick(view));
 
         // Inflate the layout for this fragment
         return rootView;
@@ -101,7 +97,40 @@ public class SynonymExpandableListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
     }
+
+    public void onNewBaseWordClick(View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        builder.setTitle("Add New Synonym");
+
+        LayoutInflater inflater = (LayoutInflater) view.getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        final View dialoglayout = inflater.inflate(R.layout.synonym_new_baseword_alert, null);
+
+        builder.setView(dialoglayout);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                EditText wordTxt = dialoglayout.findViewById(R.id.new_word_word_input);
+
+                String word = wordTxt.getText().toString().toLowerCase().trim();
+                listAdapter.addBaseWord(word);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+
+        builder.show();
+    }
+
 
 }
